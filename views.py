55d5b49d3c02910fecef9109f4ea8647dbd89e30
Rupesh-thakur <63049821+Rupesh-thakur@users.noreply.github.com>
2021-12-07 12:43:36 +0530

    new commit

--- views.py
+++ views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 from .models.product import Product
 from .models.category import Category
+from .models.customer import Customer
+
 
 # Create your views here.
 def index(request):
@@ -18,4 +20,49 @@ def index(request):
     return render(request , 'index.html' , data )
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method=='GET':
+        return render(request,'signup.html')
+    else:
+        postData = request.POST
+        first_name = postData.get('firstname')
+        last_name = postData.get('lastname')
+        phone = postData.get('phone')
+        email = postData.get('email')
+        password = postData.get('password')
+
+
+# validatin
+        error_message = None
+
+        if(not first_name):
+            error_message = "First Name Required !! "
+        elif len(first_name) < 4:
+            error_message = 'First Name must be 4 char long or more'
+        elif not last_name:
+            error_message = 'Last Name Required'
+        elif len(last_name) < 4:
+            error_message = 'Last Name must be 4 char long or more'
+        elif not phone:
+            error_message = "Phone Number Required"
+        elif len(phone) < 10:
+            error_message = 'Phone Number must be 10 char Long'
+        elif len(password) < 6:
+            error_message = 'Password must be 6 char long'
+        elif len(email) < 5:
+            error_message = 'Email must be 5 char long'
+
+
+
+        #saving
+        if not error_message:
+             print(first_name , last_name , phone , email ,password)
+             customer = Customer(first_name = first_name,
+                                 last_name = last_name,
+                                 phone = phone,
+                                 email = email,
+                                 password= password )
+             customer.register()
+        else:
+            return render(request , 'signup.html' , {'error' : error_message})
+
+    
